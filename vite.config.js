fc75979f04b27e3c4970ed2edbc2d4ab1821af63
camilo <urregocamilo604@gmail.com>
2026-07-15 13:44:21 -0500

    fix: base path para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: "/portafolio01/",
   plugins: [react()],
+  base: '/portafolio01/',
 })
